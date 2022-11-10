7aa816e65be70da8c7136138504ce5e4de2a7865
SquareThings <Amy_Xiao@Brown.edu>
2022-11-10 18:04:20 -0500

    Lab workshop2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -9,23 +10,25 @@ bakeryData.forEach((item) => {
 /* ############################################################## */
 
 function App() {
-  // TODO: use useState to create a state variable to hold the state of the cart
-  /* add your cart state code here */
+  const [cart, setCart] = useState({});
+  const [total, setTotal] = useState(0);
 
   return (
     <div className="App">
       <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
-
       {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+        <BakeryItem{...item} cart={cart} setCart={setCart} total={total} setTotal={setTotal} /> // replace with BakeryItem component
       ))}
 
       <div>
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {Object.keys(cart).map((key, index) => <div>{cart[key]}x {key}</div>)}
+
+        <h1>Total: ${(total).toFixed(2)}</h1>
       </div>
     </div>
+
   );
-}
+};
 
 export default App;
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,41 @@
-// TODO: create a component that displays a single bakery item
+
+import React, { useState } from 'react';
+import "../App.css";
+
+export default function bakeryItem(props) {
+
+    function handleClick() {
+        props.setCart(addToCart(props.cart, props.name))
+        props.setTotal(addToTotal(props.total, props.price))
+    };
+
+
+    function addToCart(cart, name) {
+        if (cart.hasOwnProperty(name)) {
+            cart[name] += 1
+        }
+        else {
+            cart[name] = 1
+        }
+        return cart
+    };
+
+    function addToTotal(total, price) {
+        return total += price
+    };
+
+
+    return <div>
+        <h1>{props.name}</h1>
+        <img src={props.image}></img>
+        <p>{props.description}</p>
+        <p>{props.price}</p>
+        <button onClick={handleClick}>Add
+        </button>
+    </div>
+
+
+}
+
+
+
